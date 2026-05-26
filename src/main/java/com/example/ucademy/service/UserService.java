3481ddbb7b36e6c79a28d3d2e991e813d60ee514
StaZarycki <stazarycki@gmail.com>
2026-05-26 22:03:33 +0200

    feat: get user by id (#3)

--- src/main/java/com/example/ucademy/service/UserService.java
+++ src/main/java/com/example/ucademy/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -51,5 +52,12 @@ public class UserService {
         List<User> users = userRepository.findAll();
 
         return users.stream().map(this::mapToResponseDto).collect(Collectors.toList());
+    }
+
+    public UserResponseDto getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User with this id does not exist"));
+
+        return mapToResponseDto(user);
     }
 }
