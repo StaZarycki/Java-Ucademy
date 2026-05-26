fd762a9acc23df417389e773461765eed4ecdeba
StaZarycki <stazarycki@gmail.com>
2026-05-26 21:51:07 +0200

    feat: get all users (#2)

--- src/main/java/com/example/ucademy/controller/UserController.java
+++ src/main/java/com/example/ucademy/controller/UserController.java
@@ -8,10 +8,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,5 +27,11 @@ public class UserController {
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody CreateUserDto dto) {
         UserResponseDto response = userService.createUser(dto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> response = userService.getAllUsers();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
